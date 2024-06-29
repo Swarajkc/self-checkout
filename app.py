af358313bc322cab8cc963b05f7eabccca2b0728
Swarajkc <swarajkc2002@gmail.com>
2024-06-29 18:26:20 +0545

    Update app.py

--- app.py
+++ app.py
@@ -1,43 +1,31 @@
-from flask import Flask, request, jsonify, render_template
-from flask_sqlalchemy import SQLAlchemy
-import os
-from werkzeug.utils import secure_filename
+# app.py
+from flask import Flask, jsonify, render_template
+import edge_detection
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
-app.config['UPLOAD_FOLDER'] = 'uploads/'
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-db = SQLAlchemy(app)
-
-class Item(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(100), nullable=False)
-    weight = db.Column(db.Float, nullable=False)
-    price = db.Column(db.Float, nullable=False)
-    image_url = db.Column(db.String(300), nullable=False)
-
-@app.route('/upload_image', methods=['POST'])
-def upload_image():
-    file = request.files['image']
-    if file:
-        filename = secure_filename(file.filename)
-        file_path = os.path.join(app.config['UPLOAD_FOLDER'], filename)
-        file.save(file_path)
-        return jsonify({"url": file_path}), 200
-
-@app.route('/add_item', methods=['POST'])
-def add_item():
-    data = request.get_json()
-    new_item = Item(name=data['label'], weight=data['weight'], price=data['total_price'], image_url=data['url'])
-    db.session.add(new_item)
-    db.session.commit()
-    return jsonify({"status": "success", "item_id": new_item.id}), 200
 
 @app.route('/')
-def index():
-    items = Item.query.all()
-    return render_template('index.html', items=items)
+def home():
+    return render_template('index.html')
+
+@app.route('/start_detection')
+def start_detection():
+    edge_detection.initialize_threads()
+    return jsonify({"status": "Detection started"}), 200
+
+@app.route('/get_details')
+def get_details():
+    price_per_gram = {
+        "lays": 1.4,
+        "coke": 0.5
+    }
+    weight = float(edge_detection.latest_weight.split()[-1])
+    price = weight * price_per_gram.get(edge_detection.latest_object.lower(), 0)
+    return jsonify({
+        "object": edge_detection.latest_object,
+        "weight": edge_detection.latest_weight,
+        "price": f"{price:.2f} Rs"
+    }), 200
 
 if __name__ == "__main__":
-    db.create_all()
-    app.run(host='0.0.0.0', port=5000)
+    app.run(debug=True, host='0.0.0.0', port=5000)
